550f40cf9b4c29ff9191eae9ebe89cd8e311403f
Robert Ramsey <rramsey@fee.org>
2015-09-09 12:43:53 -0400

    Remove legacy messaging publication

--- server/Publish.js
+++ server/Publish.js
@@ -1,22 +1,13 @@
-Accounts.onCreateUser(function(options, user) {
-	
+Accounts.onCreateUser(function(options, user) {	
 	if (options.profile)
     user.profile = options.profile;
-	user.profile = {picture: "img/default.jpg", following: []};
+	user.profile = {picture: "img/default.png", following: []};
 	return user;
 });
 
-
-Meteor.publish('messaging', function () {
-	return Meteor.users.find();
-});
-
-
-// Build out composite for this
 Meteor.publish('messagesInbox', function (sessionId) {
 	return [Messages.find({recieving: this.userId}), Texts.find({messageId: sessionId, recieving: this.userId})];
 });
-
 
 Meteor.publish('userprofile', function (id) {
 	return Posts.find({associated: {$in: id}});
@@ -26,7 +17,6 @@ Meteor.publish('profile', function (username) {
 	return [Posts.find({associated: username}), Meteor.users.find({username: username})];
 });
 
-
 Meteor.publish('favorites', function (username){
 	return Posts.find({favoriters: username});
 });
@@ -35,12 +25,10 @@ Meteor.publish('tweets', function (_id) {
 	return Posts.find({_id: _id});
 });
 
-//For the hashtags page
 Meteor.publish('hashtags', function (hashtag) {
 	return Posts.find({hashtags: hashtag});
 });
 
-//Notifications for specific user
 Meteor.publish('notifications', function () {
 	return Notifications.find({recipient: this.userId});
 });
