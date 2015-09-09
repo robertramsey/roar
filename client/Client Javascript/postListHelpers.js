9386800da5bf9026d20b4956752131f53f50ae47
Robert Ramsey <rramsey@fee.org>
2015-09-09 14:39:08 -0400

    Fix profile picture error

--- client/Client Javascript/postListHelpers.js
+++ client/Client Javascript/postListHelpers.js
@@ -1,11 +1,5 @@
 Template.postsList.helpers({
 	image: function () {
-		var picture = Meteor.user().profile.picture.image;
-		var initial = "img/default.jpg"
-		if (picture !== undefined) {
-			return picture;
-		} else {
-			return initial
-		}
+		return Meteor.user().profile.picture;
 	}
-})
+});
